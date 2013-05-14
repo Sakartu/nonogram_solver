1b14599d2dba71f331afcd632f7dbf04d56a93da
Peter Wagenaar <sakartu@gmail.com>
2013-05-14 09:32:40 +0200

    Restructure imports

--- solve.py
+++ solve.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import copy
-import argparse
-import logging
 from collections import namedtuple
 from board import NonogramBoard
+import argparse
+import logging
+import sys
+import copy
 
 
 def parse_args():
